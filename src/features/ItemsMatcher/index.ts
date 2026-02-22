b38f33fbf109cb29e621a7e0d2c91c5a90aeda97
Kiryl Vasilyeu <munusmortem@gmail.com>
2026-02-22 23:29:53 +0100

    feat: move item matcher old implementation

--- src/features/ItemsMatcher/index.ts
+++ src/features/ItemsMatcher/index.ts
@@ -1,2 +1,1 @@
-export { default as ItemsMatcher } from './ItemsMatcher'
-export type { MatchedItem, File1Item, File2Item, MatchType, FilterType, Stats } from './ItemsMatcher.types'
+export { ItemsMatcher } from './ItemsMatcher';
